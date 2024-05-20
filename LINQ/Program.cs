cbf58bc4e7113fa22bc46e4e1a214e58439334c5
JP Mohr <James.Mohr2.0@hotmail.com>
2024-05-20 10:12:56 -0400

    LINQ exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> names = new List<string> { "Alice", "Bob", "Charlie", "Danny", "Edward", "Franklin", "Greg", "Hampton" };
+            IEnumerable<string> namesByLength = names.OrderBy(name => name.Length);
+            foreach( string name in namesByLength)
+            Console.WriteLine(name);
         }
+
+        
     }
 }
